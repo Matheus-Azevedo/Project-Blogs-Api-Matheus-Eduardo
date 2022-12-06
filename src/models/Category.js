6131b34c03def2d48cdab0dbe07c451a77d11b62
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2022-12-06 17:08:42 -0300

    FEAT: Requisito 07

---
+++ src/models/Category.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    timestamps: false,
+    underscored: true,
+  });
+
+  return Category;
+}
